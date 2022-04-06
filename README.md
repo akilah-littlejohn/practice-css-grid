f5a337118d3ada8bda45cb328813bab5bf4e06d2
wisdomkeeper <akilahlittlejohn@gmail.com>
2022-04-06 12:08:42 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-ivy-yv5slx
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-ivy-yv5slx)
---
+++ src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component, VERSION } from '@angular/core';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: [ './app.component.css' ]
+})
+export class AppComponent  {
+  pics:string[] = [
+    "https://images.unsplash.com/photo-1550745165-9bc0b252726f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
+
+    "https://images.unsplash.com/photo-1487058792275-0ad4aaf24ca7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
+  ]
+
+}
